278c8746cc5b7206a8becdb01cdab9dd1bce5a7b
Tasneem_jarrar <tasneem.a.jarrar@gmail.com>
2025-12-27 16:00:57 +0200

    create details page

---
+++ assets/js/detilas.js
@@ -0,0 +1,19 @@
+const getUser = async () => {
+  const params = new URLSearchParams(location.search);
+  const userId = params.get("userId");
+  const response = await axios.get(
+    `http://ums12.runasp.net/api/users/${userId}`
+  );
+  return response.data;
+};
+const displayUserData = async () => {
+  const response = await getUser();
+  console.log(response);
+  document.querySelector(".user .details .userName").textContent = response.data.name;
+  document.querySelector(".user .details .userEmail").textContent = response.data.email;
+  document.querySelector(".user .details .userAge").textContent = response.data.age;
+  if (response.data.image) {
+    document.querySelector(".user .details .userImg").src = response.data.image;
+  }
+};
+displayUserData();
